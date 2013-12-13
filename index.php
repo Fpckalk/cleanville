e6c5706a4c7abd9b3ddb39fad197ccfc0192d0c4
fpckalk <fpckalk@gmail.com>
2013-12-13 15:33:47 +0100

    Added dashboard styling

--- index.php
+++ index.php
@@ -8,49 +8,49 @@
 					<ul>
 						<li>
 							<a href="schoondorp.php">
-								<img src="http://lorempixel.com/105/105" alt="">
+								<img src="img/icons/schoondorp.png" alt="">
 								<span>Play Schoondorp</span>
 							</a>
 						</li>
 						<li>
 							<a href="stats.php#/summary">
-								<img src="http://lorempixel.com/105/105" alt="">
+								<img src="img/icons/data.png" alt="">
 								<span>Data Usage</span>
 							</a>
 						</li>
 						<li>
 							<a href="messages.php">
-								<img src="http://lorempixel.com/105/105" alt="">
+								<img src="img/icons/messages.png" alt="">
 								<span>Messages</span>
 							</a>
 						</li>
 						<li>
 							<a href="profile.php#/profile">
-								<img src="http://lorempixel.com/105/105" alt="">
+								<img src="img/icons/profile.png" alt="">
 								<span>Profile</span>
 							</a>
 						</li>
 						<li>
 							<a href="#">
-								<img src="http://lorempixel.com/105/105" alt="">
+								<img src="img/icons/magazine.png" alt="">
 								<span>Magazine</span>
 							</a>
 						</li>
 						<li>
 							<a href="explanation.php">
-								<img src="http://lorempixel.com/105/105" alt="">
+								<img src="img/icons/explanation.png" alt="">
 								<span>Explanation</span>
 							</a>
 						</li>
 						<li>
 							<a href="settings.php#/general">
-								<img src="http://lorempixel.com/105/105" alt="">
+								<img src="img/icons/settings.png" alt="">
 								<span>Settings</span>
 							</a>
 						</li>
 						<li>
 							<a href="sensors.php">
-								<img src="http://lorempixel.com/105/105" alt="">
+								<img src="img/icons/sensors.png" alt="">
 								<span>Sensors</span>
 							</a>
 						</li>
